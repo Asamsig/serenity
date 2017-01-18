19362e0c239226fa25f3cf22cde36cc08423182f
Ingar Abrahamsen <ingar@abrahams1.com>
2017-01-18 01:27:30 +0100

    chore: add silhouette dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 
 object Versions {
   val akka = "2.4.11"
+  val silhouette = "4.0.0"
   val scalaVersion = "2.11.8"
 }
 
@@ -23,7 +24,20 @@ object Dependencies {
     "com.github.dnvriend"    %% "akka-persistence-inmemory" % "1.3.18" % "test"
   )
 
-  val playDependencies = akka ++ test
+  private val silhouette = Seq(
+    "com.mohiva" %% "play-silhouette" % Versions.silhouette,
+    "com.mohiva" %% "play-silhouette-password-bcrypt" % Versions.silhouette,
+    "com.mohiva" %% "play-silhouette-persistence" % Versions.silhouette,
+    "com.mohiva" %% "play-silhouette-crypto-jca" % Versions.silhouette,
+    "com.mohiva" %% "play-silhouette-testkit" % Versions.silhouette % "test"
+  )
 
-  val logicDependencies = akka ++ persistence ++ test
+  private val root = Seq(
+    "com.iheart" %% "ficus" % "1.2.6",
+    "net.codingwell" %% "scala-guice" % "4.0.1"
+  )
+
+  val playDependencies = akka ++ test ++ silhouette ++ root
+
+  val logicDependencies = akka ++ persistence ++ test ++ silhouette
 }
