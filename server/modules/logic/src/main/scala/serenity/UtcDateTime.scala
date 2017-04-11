4bd949f1308a2704ab6c3536486b7dc690a9f840
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-11 23:31:59 +0200

    feat: time and date helpers
    Helper functions to ensure utc time

--- server/modules/logic/src/main/scala/serenity/UtcDateTime.scala
+++ server/modules/logic/src/main/scala/serenity/UtcDateTime.scala
@@ -1,9 +1,25 @@
 package serenity
 
-import java.time.{Clock, LocalDateTime}
+import java.time._
 
 object UtcDateTime {
 
-  def nowUTC(): LocalDateTime = LocalDateTime.now(Clock.systemUTC())
+  def nowUTC(): LocalDateTime = time.dateTimeNow()
+
+}
+
+object time {
+
+  def dateTimeNow(): LocalDateTime =
+    LocalDateTime.now(Clock.systemUTC())
+
+  def toUtcMillis(ld: LocalDate) =
+    ld.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli
+
+  def instanceToUtcLocalDate(ins: Instant): LocalDate =
+    LocalDateTime.ofEpochSecond(ins.getEpochSecond, ins.getNano, ZoneOffset.UTC).toLocalDate
+
+  def instanceToUtcLocalDateTime(ins: Instant): LocalDateTime =
+    LocalDateTime.ofEpochSecond(ins.getEpochSecond, ins.getNano, ZoneOffset.UTC)
 
 }
