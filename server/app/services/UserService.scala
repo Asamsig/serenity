854827f8b2d8f081b11a0b960f57fd3986ac4e17
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-25 23:28:40 +0200

    refactor: use UserRepository in UserService

--- server/app/services/UserService.scala
+++ server/app/services/UserService.scala
@@ -1,65 +1,39 @@
 package services
 
-import javax.inject.{Inject, Named}
+import javax.inject.Inject
 
-import akka.actor.ActorRef
-import akka.pattern.ask
-import akka.util.Timeout
-import models.user.Auths.BasicAuth
+import models.user.Auths.{BasicAuth, SerenityAuth}
 import models.user.User
 import play.api.Logger
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import repositories.eventsource.users.UserReadProtocol._
-import repositories.eventsource.users.UserWriteProtocol.UpdateCredentialsCmd
+import repositories.view.UserRepository
 
 import scala.concurrent.Future
-import scala.concurrent.duration.DurationDouble
-import scala.util.control.NonFatal
 
-class UserService @Inject()(@Named("UserManagerActor") userManagerActor: ActorRef) {
-
-  implicit val timeout: Timeout = 120.seconds
+class UserService @Inject()(userRepository: UserRepository) {
 
   val logger = Logger(classOf[UserService])
 
-  def findUser(email: String): Future[Option[User]] =
-    (userManagerActor ? GetUserWithEmail(email)).map {
-      case UserResponse(usr) =>
-        logger.debug(s"Found user matching email $email -> ${usr.userId}")
-        Some(usr)
-
-      case _ =>
-        logger.debug(s"No matching user for $email")
-        None
-
-    }.recover { case NonFatal(t) =>
-      logger.warn(s"Unable to retrieve user for email $email", t)
-      None
-    }
+  def findUser(email: String): Future[Option[User]] = {
+    userRepository.findUserIdByEmail(email)
+        .flatMap {
+          case Some(uid) => userRepository.fetchUserById(uid)
+          case None => Future.successful(None)
+        }
+  }
 
   def findAuth(email: String): Future[Option[BasicAuth]] = {
     logger.info(s"requesting cred for user $email ")
-    (userManagerActor ? GetUserCredentials(email)).map {
-      case UserCredentialsResponse(auth) =>
-        logger.debug(s"Found credentials for user with email $email")
-        Some(auth)
-
-      case CredentialsNotFound =>
-        logger.debug(s"No credentials found for user with $email")
-        None
-
-      case m =>
-        logger.warn(s"No credentials found for user with $email." +
-            s" Found an unexpected response of type ${m.getClass}")
-        None
-    }
+    userRepository.credentialsByEmail(email)
   }
 
   def updateCredentials(email: String, hashedPassword: String): Future[BasicAuth] = {
-    (userManagerActor ? UpdateCredentialsCmd(email, hashedPassword)).map {
-      case UserCredentialsResponse(auth) =>
-        logger.debug(s"Updated credentials for user with email $email")
-        auth
+    userRepository.findUserIdByEmail(email).flatMap {
+      case Some(uid) =>
+        val auth = SerenityAuth(hashedPassword)
+        userRepository.saveCredentials(uid, auth).map(_ => auth)
+      case None =>
+        Future.failed(new IllegalStateException(s"No user with the give email $email"))
     }
   }
 
