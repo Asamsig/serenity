a07e3d91721d91cffb0370ec3b276dac902c8875
Ingar Abrahamsen <ingar@abrahams1.com>
2017-05-08 23:18:00 +0200

    refactor: use named parameters in the mapping of enum to path

--- server/app/controllers/helpers/EnumPathExtractor.scala
+++ server/app/controllers/helpers/EnumPathExtractor.scala
@@ -6,8 +6,8 @@ import play.api.routing.sird.PathBindableExtractor
 object EnumPathExtractor {
   def binders[E <: Enumeration](enum: E): PathBindableExtractor[E#Value] = {
     val pb = new PathBindable.Parsing[E#Value](
-      enum.withName(_),
-      (e: E#Value) => e.toString,
+      str => enum.withName(str),
+      enum => enum.toString,
       (k: String, e: Exception) =>
         "Cannot parse %s as %s: %s".format(k, enum.getClass.getName, e.getMessage)
     )
